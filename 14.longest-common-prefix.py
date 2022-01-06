d119cb1b9ebf9b2e44453cd5ed1ec7240699227a
naresh nishad <nc.nishad10@gmail.com>
2022-01-06 15:36:41 +0530

    jan-6

--- 14.longest-common-prefix.py
+++ 14.longest-common-prefix.py
@@ -11,12 +11,7 @@ from typing import List
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         Length = len(strs)
-        if Length == 0:
-            return ""
-        elif Length == 1:
-            return strs[0]
-        else:
-            prefix = strs[0]
+
 
 
 
