e91851697e7918cb71f4e35c5c22d319eab83d29
naresh nishad <nc.nishad10@gmail.com>
2022-01-22 14:34:38 +0530

    jan-22

---
+++ 409.longest-palindrome.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=409 lang=python3
+#
+# [409] Longest Palindrome
+#
+
+# @lc code=start
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        hash = set()
+        for c in s:
+            if c not in hash:
+                hash.add(c)
+            else:
+                hash.remove(c)
+        # len(hash) is the number of the odd letters
+        return len(s) - len(hash) + 1 if len(hash) > 0 else len(s)
+
+        
+# @lc code=end
+
