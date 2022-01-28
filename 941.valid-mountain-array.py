9da0927518baedf69de2c81f074622eca35ec237
naresh nishad <nc.nishad10@gmail.com>
2022-01-28 09:13:22 +0530

    jan-28

---
+++ 941.valid-mountain-array.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=941 lang=python3
+#
+# [941] Valid Mountain Array
+#
+
+# @lc code=start
+from re import T
+
+
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        if len(arr) < 3:
+            return False
+        increasing = True
+        for i in range(len(arr)-1):
+            if arr[i+1] < arr[i]:
+                increasing = False
+            elif increasing and arr[i+1] <= arr[i]:
+                return False
+            elif not increasing and arr[i+1] >= arr[i]:
+                return False
+        return True and not increasing
+        
+# @lc code=end
+
