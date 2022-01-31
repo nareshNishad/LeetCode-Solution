2af6665fda1735b8e26377250e957fda01be5579
naresh nishad <nc.nishad10@gmail.com>
2022-01-31 10:04:26 +0530

    jan-31

--- 5.longest-palindromic-substring.py
+++ 5.longest-palindromic-substring.py
@@ -7,14 +7,20 @@
 # @lc code=start
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) <=1:
-            return s
-        out = ""
-        for i in range(len(s)):
-            for j in range(i, len(s)+1):
-                if s[i:j] == s[i:j][::-1] and len(out) < len(s[i:j]):
-                    out = s[i:j]
-        return out
+        res = ""
+        for i in range(len(s)):        
+            odd  = self.palindromeAt(s, i, i)
+            even = self.palindromeAt(s, i, i+1)
+            
+            res = max(res, odd, even, key=len)
+        return res
+ 
+# starting at l,r expand outwards to find the biggest palindrome
+    def palindromeAt( self, s, l, r):    
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        return s[l+1:r]
         
 # @lc code=end
 
