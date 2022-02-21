3b8f9d96668f75a0c369bdb448b6e0744288a898
naresh nishad <nc.nishad10@gmail.com>
2022-02-21 10:03:28 +0530

    feb-21

--- 11.container-with-most-water.py
+++ 11.container-with-most-water.py
@@ -7,6 +7,13 @@
 # @lc code=start
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        out = 0
+        for i, length in enumerate(height):
+            for j, breath in enumerate(height[i:]):
+                out = max(min(length, breath)*(j-i), out)
+        return out
+
+
         
 # @lc code=end
 
--- 237.delete-node-in-a-linked-list.py
+++ 237.delete-node-in-a-linked-list.py
@@ -13,6 +13,8 @@
 
 class Solution:
     def deleteNode(self, node):
+        node.val = node.next.val
+        node.next = node.next.next
         """
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
